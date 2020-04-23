27f4a4498826b290fd2a25a13d79009733f51faf
gcompiano <gucompi@gmail.com>
2020-04-22 23:54:49 -0300

    Cambio para que se adapte a lo ultimo de babel y webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,25 @@
+const path = require("path");
 
-const path = require('path'); // path utility
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// init HTML Webpack Plugin
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
- template: './public/index.html', // archivo de nuestra vista
- inject: 'body' // donde insertaremos nuestro script
-})
-const config = {
- entry: './src/index.js', // archivo js que codearemos
- output: {
-  path: path.resolve('./public'), //resolver el path de salida
-  filename: 'bundle.js' // archivo js compilado
- },
- module: {
-  loaders: [
-   {
-    test: /\.js$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/
-   },
-   {
-    test: /\.jsx$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/
-   }
-  ]
- },
- resolve: {
-  extensions: ['.js', '.jsx']
- },
- plugins: [HtmlWebpackPluginConfig] // configuración de nuestra vista
-}
-module.exports = config; //exportamos a webpack nuestra configuración
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    port: 9000
+  }
+};
